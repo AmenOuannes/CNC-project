87f85ac1be7977d19fb310077be9b6e27c757e00
zieddaly <zied.daly.1@ulaval.ca>
2024-10-11 17:00:35 -0400

    Ajout  App exe

---
+++ equipe06/src/main/java/NewMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ziedd
+ */
+
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Atelier.gui.MainWindow mainWindow = new Atelier.gui.MainWindow();
+        mainWindow.setVisible(true);
+    }
+    
+}
