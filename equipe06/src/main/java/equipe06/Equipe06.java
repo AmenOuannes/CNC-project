0ec87bdb3ea2fdf62f528582dec879e77ca68131
zieddaly <zied.daly.1@ulaval.ca>
2024-10-28 22:19:09 -0400

    Ajout Mainwindow

---
+++ equipe06/src/main/java/equipe06/Equipe06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package equipe06;
+
+/**
+ *
+ * @author ziedd
+ */
+public class Equipe06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- equipe06/src/main/java/equipe06/Main.java
+++ equipe06/src/main/java/equipe06/Main.java
@@ -2,22 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package equipe06;
 
 /**
  *
- * @author equipe6
+ * @author ziedd
  */
-
-
-public class NewMain {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Atelier.gui.MainWindow mainWindow = new Atelier.gui.MainWindow();
+        equipe06.gui.MainWindow mainWindow = new equipe06.gui.MainWindow();
         mainWindow.setVisible(true);
+        
     }
     
-}  
+}
