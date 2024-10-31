09eac1591ff97ffadf5fb65d29731b4c37e3f98e
Mohamed Hedi Braham <mohamed-hedi.braham.1@ulaval.ca>
2024-10-30 20:05:16 -0400

    class element coupe + point

---
+++ equipe06/src/main/java/equipe06/Domaine/ElementCoupe.java
@@ -0,0 +1,114 @@
+package equipe06.Utils;
+import equipe06.Domaine.Utils.Point;
+
+/**
+ *
+ * @author hedi, katia
+ */
+
+
+public class ElementCoupe {
+
+    // Attributs de la classe
+    private Point pointOrigine;
+    private Point pointDestination;
+    private float profondeur;
+    private float marge;
+    private float axe;
+    private boolean composante;
+    private float bordureX;
+    private float bordureY;
+    private String typeCoupe;
+
+    // Constructeur par défaut
+    public ElementCoupe() {
+    }
+
+    // Constructeur avec paramètres pour initialiser tous les attributs
+    public ElementCoupe(Point pointOrigine, Point pointDestination, float profondeur, float marge, float axe,
+                        boolean composante, float bordureX, float bordureY, String typeCoupe) {
+        this.pointOrigine = pointOrigine;
+        this.pointDestination = pointDestination;
+        this.profondeur = profondeur;
+        this.marge = marge;
+        this.axe = axe;
+        this.composante = composante;
+        this.bordureX = bordureX;
+        this.bordureY = bordureY;
+        this.typeCoupe = typeCoupe;
+    }
+
+    // Getters et setters pour chaque attribut
+    public Point getPointOrigine() {
+        return pointOrigine;
+    }
+
+    public void setPointOrigine(Point pointOrigine) {
+        this.pointOrigine = pointOrigine;
+    }
+
+    public Point getPointDestination() {
+        return pointDestination;
+    }
+
+    public void setPointDestination(Point pointDestination) {
+        this.pointDestination = pointDestination;
+    }
+
+    public float getProfondeur() {
+        return profondeur;
+    }
+
+    public void setProfondeur(float profondeur) {
+        this.profondeur = profondeur;
+    }
+
+    public float getMarge() {
+        return marge;
+    }
+
+    public void setMarge(float marge) {
+        this.marge = marge;
+    }
+
+    public float getAxe() {
+        return axe;
+    }
+
+    public void setAxe(float axe) {
+        this.axe = axe;
+    }
+
+    public boolean getComposante() {
+        return composante;
+    }
+
+    public void setComposante(boolean composante) {
+        this.composante = composante;
+    }
+
+    public float getBordureX() {
+        return bordureX;
+    }
+
+    public void setBordureX(float bordureX) {
+        this.bordureX = bordureX;
+    }
+
+    public float getBordureY() {
+        return bordureY;
+    }
+
+    public void setBordureY(float bordureY) {
+        this.bordureY = bordureY;
+    }
+
+    public String getTypeCoupe() {
+        return typeCoupe;
+    }
+
+    public void setTypeCoupe(String typeCoupe) {
+        this.typeCoupe = typeCoupe;
+    }
+
+}
