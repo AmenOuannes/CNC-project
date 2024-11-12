89b5d1e2c3bf1dbcec51eb6beef4e417602ea8d9
zieddaly <zied.daly.1@ulaval.ca>
2024-11-12 03:54:28 -0500

    commentaire supp outil

--- equipe06/src/main/java/equipe06/gui/MainWindow.java
+++ equipe06/src/main/java/equipe06/gui/MainWindow.java
@@ -652,8 +652,7 @@ public class MainWindow extends javax.swing.JFrame {
     }//GEN-LAST:event_Nom_OutilActionPerformed
 
     private void Supprimer_OutilActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Supprimer_OutilActionPerformed
-        // Suppression d'un outil sélectionné dans la liste
-        
+        // Suppression d'un outil sélectionné dans le jtable
         int index = tableauOutils.getSelectedRow();
         if (index != -1) {
             controleur.supprimerOutil(index); // Supprime l'outil dans CNC via le contrôleur
@@ -666,7 +665,8 @@ public class MainWindow extends javax.swing.JFrame {
         
         
         // code de fares avec le jlist cree on va utiliser un jtable donc ce n'est pas la meme chose 
-        // jlist pour un seul champ et on pourra pas faire des mise a jour dynamique avec le jlist 
+        // jlist pour un seul champ et on pourra pas faire des mise a jour dynamique avec le jlist
+        // le jlist de fares existe encore et dans l'interface et fonctionne aussi 
         int selectedIndex = Liste_Outil.getSelectedIndex();
         if (selectedIndex != -1) {
         String outilASupprimer = outilsListModel.getElementAt(selectedIndex);
