1d65b85ba6eb9a1e2658b485f62042d93aed695f
CollectorsObservatory <fares.majdoub.1@ulaval.ca>
2024-11-19 22:16:13 -0500

    Ajout Zoom Preliminaire

--- equipe06/src/main/java/equipe06/gui/PanneauVue.java
+++ equipe06/src/main/java/equipe06/gui/PanneauVue.java
@@ -12,9 +12,9 @@ import equipe06.Domaine.PanneauDTO;
 
 public class PanneauVue extends JPanel {
     private MainWindow mainWindow;
-    public int largeurPixelsTable;  
+    public int largeurPixelsTable;
     public int hauteurPixelsTable;
-    private int largeurPixelsPanneau; 
+    private int largeurPixelsPanneau;
     private int hauteurPixelsPanneau;
     private Repere repere;
     private Controleur controleur;
@@ -22,28 +22,32 @@ public class PanneauVue extends JPanel {
     private int lastClickY = -1;
     public boolean deleteTriggered = false;
     public boolean modifyTriggered;
-    //private static final double SCALE_FACTOR = 0.09;
     private float BordureX;
     private float BordureY;
-
     private double zoomFactor = 1.0;
     private boolean peutCreerCoupe = false;
-    private boolean peutCreerCoupeRect = false ;
-    private boolean peutCreerCoupeBordure = false ;
-    private boolean peutCreerCoupeL = false ;
+    private boolean peutCreerCoupeRect = false;
+    private boolean peutCreerCoupeBordure = false;
+    private boolean peutCreerCoupeL = false;
     private boolean peutCreerCoupeH = false;
     private boolean peutCreerCoupeV = false;
-
 
     // Variables pour gérer le décalage de la vue lors du zoom
     private double offsetX = 0.0;
     private double offsetY = 0.0;
-    
+
     // Variable pour la coupe rect (clic)
     private int rectX1 = -1;
     private int rectY1 = -1;
     private int rectX2 = -1;
     private int rectY2 = -1;
+
+    // Variable pour la coupe L
+    private int x1 = -1;
+    private int y1 = -1;
+    private int x2 = -1;
+    private int y2 = -1;
+
     public PanneauVue(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
         this.repere = new Repere();
@@ -55,11 +59,11 @@ public class PanneauVue extends JPanel {
         this.hauteurPixelsTable = (int) (repere.convertirEnPixelsDepuisPouces(60));
 
         // Conversion des dimensions du panneau avec facteur d'échelle
-        this.largeurPixelsPanneau = (int) (repere.convertirEnPixelsDepuisMm(panneauDTO.getLargeur()) );
+        this.largeurPixelsPanneau = (int) (repere.convertirEnPixelsDepuisMm(panneauDTO.getLargeur()));
         this.hauteurPixelsPanneau = (int) (repere.convertirEnPixelsDepuisMm(panneauDTO.getLongueur()));
 
         // Définir la taille préférée du panneau basé sur la table CNC
-        this.setPreferredSize(new Dimension(largeurPixelsTable , hauteurPixelsTable ));
+        this.setPreferredSize(new Dimension(largeurPixelsTable, hauteurPixelsTable));
 
         // Ajouter un MouseListener pour la création de coupe
         this.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -67,14 +71,13 @@ public class PanneauVue extends JPanel {
                 drawingPanelMouseClicked(evt);
             }
         });
-        
+
         this.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 captureRectanglePoints(evt);
             }
         });
-         
-        
+
         // Ajouter un écouteur pour la roulette de la souris
         this.addMouseWheelListener(new MouseWheelListener() {
             @Override
@@ -109,7 +112,6 @@ public class PanneauVue extends JPanel {
                     offsetX = mouseX - (mouseX - offsetX) * scaleChange;
                     offsetY = mouseY - (mouseY - offsetY) * scaleChange;
                 }
-
                 repaint(); // Redessiner après le changement de zoom
             }
         });
@@ -124,33 +126,21 @@ public class PanneauVue extends JPanel {
     }
 
     private void drawingPanelMouseClicked(java.awt.event.MouseEvent evt) {
-        if (peutCreerCoupeH || peutCreerCoupeV) {
-            lastClickX = evt.getX();
-            lastClickY = evt.getY();
+        if (peutCreerCoupeH) {
+            lastClickX = ajusterCoordonneePourVue(evt.getX(), offsetX, zoomFactor);
+            lastClickY = ajusterCoordonneePourVue(evt.getY(), offsetY, zoomFactor);
             repaint();
-            //peutCreerCoupeH = false;
-            //peutCreerCoupeV = false;
-
-        } else {
-            /*// Zoom centré sur le clic
-            double oldZoomFactor = zoomFactor;
-            zoomFactor += 0.2;
-            if (zoomFactor > 3.0) {
-                zoomFactor = 3.0; // Limiter le zoom maximal
-            }
-
-            // Calculer la différence pour recadrer le point autour du clic
-            double scaleChange = zoomFactor / oldZoomFactor;
-            int clickX = evt.getX();
-            int clickY = evt.getY();
-
-            offsetX = clickX - (clickX - offsetX) * scaleChange;
-            offsetY = clickY - (clickY - offsetY) * scaleChange;
-
-            repaint();*/
+        } else if (peutCreerCoupeV) {
+            lastClickX = ajusterCoordonneePourVue(evt.getX(), offsetX, zoomFactor);
+            lastClickY = ajusterCoordonneePourVue(evt.getY(), offsetY, zoomFactor);
+            repaint();
         }
     }
-// TODO dessiner tout le vecteur, amen
+
+    private int ajusterCoordonneePourVue(int coordonnee, double offset, double zoomFactor) {
+        return (int) ((coordonnee - offset) / zoomFactor);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -165,121 +155,127 @@ public class PanneauVue extends JPanel {
         g2d.setColor(Color.LIGHT_GRAY);
         g2d.fillRect(0, 0, largeurPixelsTable, hauteurPixelsTable);
         g2d.setColor(Color.BLACK);
-        g2d.drawRect(0, 0, largeurPixelsTable-1, hauteurPixelsTable-1);
-
-        // Dessiner les axes X et Y
-        //dessinerAxes(g2d);
+        g2d.drawRect(0, 0, largeurPixelsTable - 1, hauteurPixelsTable - 1);
 
         // Utiliser l'Afficheur pour dessiner les autres éléments (panneau, coupes, etc.)
         Afficheur afficheur = new Afficheur(mainWindow.controleur);
         afficheur.DessinerPanneau(g, hauteurPixelsTable);
-        //for coupe in coupes if coupe kedhe afficheur.kedhe
+
+        // Dessiner les axes X et Y
+        dessinerAxes(g2d);
+
+        // Dessiner les coupes selon le zoom et le décalage
         if (modifyTriggered) {
             afficheur.dessinerCoupeModifie(g, hauteurPixelsTable);
-        }
-        if (peutCreerCoupeH){
-            System.out.printf("peutCreerCoupeH MRGLA");
+        } else if (lastClickX != -1 && lastClickY != -1) {
+            // Ajuster les coordonnées de la coupe au zoom et au décalage
+            int adjustedX = ajusterCoordonneePourVue(lastClickX, offsetX, zoomFactor);
+            int adjustedY = ajusterCoordonneePourVue(lastClickY, offsetY, zoomFactor);
 
-            afficheur.dessinerCoupe(g, lastClickX, lastClickY, hauteurPixelsTable, largeurPixelsTable, false);
+            // S'assurer que la coupe reste dans les limites du panneau (zone orange)
+            adjustedX = Math.max(0, Math.min(adjustedX, largeurPixelsPanneau - 1));
+            adjustedY = Math.max(hauteurPixelsTable - hauteurPixelsPanneau, Math.min(adjustedY, hauteurPixelsTable - 1));
+
+            if (peutCreerCoupeH) {
+                afficheur.dessinerCoupe(g, adjustedX, adjustedY, hauteurPixelsTable, largeurPixelsTable, false);
+            }
+            if (peutCreerCoupeV) {
+                afficheur.dessinerCoupe(g, adjustedX, adjustedY, hauteurPixelsTable, largeurPixelsTable, true);
+            }
         }
-        if (peutCreerCoupeV){
-            System.out.printf("peutCreerCoupeV MRGLA");
-            afficheur.dessinerCoupe(g, lastClickX, lastClickY, hauteurPixelsTable, largeurPixelsTable, true);
+
+        if (peutCreerCoupeRect && rectX1 != -1 && rectY1 != -1 && rectX2 != -1 && rectY2 != -1) {
+            int adjustedX1 = rectX1;
+            int adjustedY1 = rectY1;
+            int adjustedX2 = rectX2;
+            int adjustedY2 = rectY2;
+            afficheur.dessinerRectangleAVdeuxpoints(g, adjustedX1, adjustedY1, adjustedX2, adjustedY2);
         }
-        if (peutCreerCoupeRect) {
-            afficheur.dessinerRectangleAVdeuxpoints(g, rectX1, rectY1, rectX2, rectY2);
+
+        if (peutCreerCoupeL && x1 != -1 && y1 != -1 && x2 != -1 && y2 != -1) {
+            int adjustedX1 = x1;
+            int adjustedY1 = y1;
+            int adjustedX2 = x2;
+            int adjustedY2 = y2;
+            afficheur.dessinerL(g, adjustedX1, adjustedY1, adjustedX2, adjustedY2);
         }
-        if (peutCreerCoupeL) {
-          afficheur.dessinerL(g, rectX1, rectY1, rectX2, rectY2);
-         }
+
         if (peutCreerCoupeBordure) {
-            afficheur.dessinerBordure(g, BordureX, BordureY, hauteurPixelsTable);
+            float adjustedBordureX = BordureX;
+            float adjustedBordureY = BordureY;
+            afficheur.dessinerBordure(g, adjustedBordureX, adjustedBordureY, hauteurPixelsTable);
         }
-
-        // Réinitialiser après dessin
-        lastClickX = -1;
-        lastClickY = -1;
-        rectX1 = -1;
-        rectY1 = -1;
-        rectY1 = -1;
-        rectY2 = -1;
-        modifyTriggered = false;
-        peutCreerCoupeRect = false;
-        peutCreerCoupeBordure = false;
-        peutCreerCoupeL = false;
     }
 
     private void dessinerAxes(Graphics g) {
         g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.PLAIN, 12));
+        FontMetrics metrics = g.getFontMetrics();
 
-        // Dessiner l'axe X (horizontal)
-        int xStart = 50;
-        int yPosition = 50 + hauteurPixelsTable;
-        g.drawLine(xStart, yPosition, xStart + largeurPixelsTable, yPosition);
+        // Axe X (horizontal, sur la bordure inférieure)
+        int yPosition = hauteurPixelsTable;
+        g.drawLine(0, yPosition, largeurPixelsTable, yPosition);
 
-        // Ajouter des graduations sur l'axe X tous les 200 mm
-        for (int i = 0; i <= 3000; i += 200) {
-            int xPos = xStart + (int) (i * 3.78  * zoomFactor);
+        for (int i = 0; i <= 3000; i += 100) {
+            int xPos = repere.convertirEnPixelsDepuisMm(i);
             g.drawLine(xPos, yPosition - 5, xPos, yPosition + 5);
-            g.drawString(String.valueOf(i), xPos - 10, yPosition + 20);
+            String texte = String.valueOf(i);
+            int texteLargeur = metrics.stringWidth(texte);
+            g.drawString(texte, xPos - texteLargeur / 2, yPosition + metrics.getHeight() + 5);
         }
 
-        // Dessiner l'axe Y (vertical)
-        int yStart = 50;
-        int xPosition = 50;
-        g.drawLine(xPosition, yStart - 130, xPosition, yStart + hauteurPixelsTable - 130);
+        // Axe Y (vertical, sur la bordure gauche)
+        int xPosition = 0;
+        g.drawLine(xPosition, 0, xPosition, hauteurPixelsTable);
 
-        // Ajouter des graduations sur l'axe Y tous les 100 mm
         for (int i = 0; i <= 1500; i += 100) {
-            int yPos = yStart + hauteurPixelsTable - (int) (i * 3.78  * zoomFactor);
+            int yPos = hauteurPixelsTable - repere.convertirEnPixelsDepuisMm(i);
             g.drawLine(xPosition - 5, yPos, xPosition + 5, yPos);
-            g.drawString(String.valueOf(i), xPosition - 45, yPos + 5);
+            String texte = String.valueOf(i);
+            int texteHauteur = metrics.getHeight();
+            g.drawString(texte, xPosition - metrics.stringWidth(texte) - 10, yPos + texteHauteur / 4);
         }
     }
-//<<<<<<< Updated upstream
 
-    // Activer la création de la coupe
-    //public void activerCreationCoupe() { 
-        // Hedi houni zid les variables teeks true false switch case
-        // starr 788 fi mainwindow
-//=======
     public void activerCreationCoupeL() {
         this.peutCreerCoupeL = true;
     }
+
     public void activerCreationCoupeH() {
-//>>>>>>> Stashed changes
         this.peutCreerCoupeH = true;
     }
+
     public void activerCreationCoupeV() {
-//>>>>>>> Stashed changes
         this.peutCreerCoupeV = true;
     }
+
     public void activerCreationCoupeRect() {
         this.peutCreerCoupeRect = true;
     }
+
     public boolean isAttenteClicPourCoupe() {
         return peutCreerCoupe;
     }
+
     private void captureRectanglePoints(java.awt.event.MouseEvent evt) {
         if (rectX1 == -1 && rectY1 == -1 && (peutCreerCoupeRect || peutCreerCoupeL)) {
-            rectX1 = evt.getX();
-            rectY1 = evt.getY();
+            rectX1 = ajusterCoordonneePourVue(evt.getX(), offsetX, zoomFactor);
+            rectY1 = ajusterCoordonneePourVue(evt.getY(), offsetY, zoomFactor);
         } else {
-            rectX2 = evt.getX();
-            rectY2 = evt.getY();
+            rectX2 = ajusterCoordonneePourVue(evt.getX(), offsetX, zoomFactor);
+            rectY2 = ajusterCoordonneePourVue(evt.getY(), offsetY, zoomFactor);
             repaint();
         }
     }
-    
+
     public void activerCreationCoupeBordure() {
         this.peutCreerCoupeBordure = true;
     }
-    
+
     public void DimensionsBordure(float BordureXValue, float BordureYValue) {
         this.BordureX = BordureXValue;
         this.BordureY = BordureYValue;
         this.peutCreerCoupeBordure = true;
         repaint();
     }
-    
 }
